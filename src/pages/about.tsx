bcd3cc2261b1f0417855d6b0283526ee55727c79
Huyu2239 <huyu2239@gmail.com>
2025-02-07 03:02:21 +0900

    fix

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -32,7 +32,7 @@ export const About = () => {
         <div className="detail">
           <h4>アクセス</h4>
           <p>広島UP-WARD (広島県広島市中区薬研堀 7-9 三和ビル 6F)</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d823.1099475114013!2d132.46386336964605!3d34.39016599831447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355aa20507d3aaab%3A0x5896f3790ec1ac49!2z5bqD5bO2VVAtV0FSRA!5e0!3m2!1sja!2sjp!4v1738861216512!5m2!1sja!2sjp" width="600" height="450" style={{"border": "0", "margin": "0 auto"}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d823.1099475114013!2d132.46386336964605!3d34.39016599831447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355aa20507d3aaab%3A0x5896f3790ec1ac49!2z5bqD5bO2VVAtV0FSRA!5e0!3m2!1sja!2sjp!4v1738861216512!5m2!1sja!2sjp" width="80%" height="450" style={{"border": "0", "margin": "0 auto"}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        </div>
 
       </div>
       <Footer />
