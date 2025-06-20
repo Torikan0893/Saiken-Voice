5436613501b15504a49e3ac9cd4a624e15168138
Torikan0893 <jamjamhakase@yahoo.co.jp>
2025-06-20 22:10:25 +0900

    演者更新

--- src/pages/cast.tsx
+++ src/pages/cast.tsx
@@ -6,50 +6,54 @@ import "../styles/cast.scss";
 type Cast = {"name": string, "twitter_name": string};
 
 const CASTS: Cast[] = [
-  // {
-  //    "name": "G-1",
-  //    "twitter_name": "nekobasi_illust",
-  // },
-  // {
-  //   "name": "G-2",
-  //   "twitter_name": "SeIenium_selen",
-  // },
-  // {
-  //   "name": "G-3",
-  //   "twitter_name": "A2_Okita_Xalter",
-  // },
+  {
+    "name": "G-1",
+    "twitter_name": "nejishiki0221",
+  },
+  {
+    "name": "G-2",
+    "twitter_name": "rinx2musixxx",
+  },
   
-  // {
-  //   "name": "D-1",
-  //   "twitter_name": "dtm_honey_music",
-  // },
-  // {
-  //   "name": "D-2",
-  //   "twitter_name": "jagkazu",
-  // },
-  // {
-  //   "name": "D-3",
-  //   "twitter_name": "menanonosekai",
-  // },
-  // {
-  //   "name": "D-4",
-  //   "twitter_name": "y2k2u1",
-  // },
-  // {
-  //   "name": "D-5",
-  //   "twitter_name": "owari_error",
-  // },
-  // {
-  //   "name": "D-6",
-  //   "twitter_name": "asikan3745",
-  // },
-  // {
-  //   "name": "D-7",
-  //   "twitter_name": "rokutii_3500",
-  // },
+  {
+    "name": "D-1",
+    "twitter_name": "M_kakoichi",
+  },
+  {
+    "name": "D-2",
+    "twitter_name": "jagkazu",
+  },
+  {
+    "name": "D-3",
+    "twitter_name": "A2_Okita_Xalter",
+  },
+  {
+    "name": "D-4",
+    "twitter_name": "y2k2u1",
+  },
+  {
+    "name": "D-5",
+    "twitter_name": "owari_error",
+  },
+  {
+    "name": "D-6",
+    "twitter_name": "asikan3745",
+  },
+  {
+    "name": "D-7",
+    "twitter_name": "rokutii_3500",
+  },
   {
     "name": "V-1",
     "twitter_name": "Torikan_0893",
+  },
+  {
+    "name": "V-2",
+    "twitter_name": "osouji_rapi",
+  },
+  {
+    "name": "V-3",
+    "twitter_name": "metameta194",
   },
 ]
 
@@ -59,7 +63,7 @@ const CasrtCard = (props: Cast) => {
       <a href={"https://x.com/" + props.twitter_name} target="_blank">
         <img src={"./cast/" + props.name + ".webp"} alt={props.name} />
       </a>  
-      {/* <p>{props.twitter_name}</p> */}
+      <p>{"@"+props.twitter_name}</p>
     </div>
   );
 }
@@ -75,7 +79,7 @@ export const Cast = () => {
         <div className="detail">
           <h4>Guest</h4>
           <p>
-          <li>調整中...</li>
+          {/* <li>調整中...</li> */}
           </p>
           <div className="CastList">
             {CASTS.filter((cast) => cast.name[0]==="G").map((cast) => <CasrtCard key={cast.name} {...cast} />)}
@@ -84,7 +88,7 @@ export const Cast = () => {
         <div className="detail">
           <h4>DJ</h4>
           <p>
-          <li>調整中...</li>
+          {/* <li>調整中...</li> */}
           </p>
           <div className="CastList">
             {CASTS.filter((cast) => cast.name[0]==="D").map((cast) => <CasrtCard key={cast.name} {...cast} />)}
@@ -96,7 +100,7 @@ export const Cast = () => {
             {CASTS.filter((cast) => cast.name[0]==="V").map((cast) => <CasrtCard key={cast.name} {...cast} />)}
           </div>
           <p>
-          <li>らぴ</li>
+          {/* <li>らぴ</li> */}
           </p>
         </div>
       </div>
